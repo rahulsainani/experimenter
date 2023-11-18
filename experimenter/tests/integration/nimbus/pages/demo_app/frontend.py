23248040cf6e39bfe9dd1fe574d2af863c4124b9
Benjamin Forehand Jr <bennyjr169@gmail.com>
2023-11-17 18:52:02 -0600

    Change function call.

--- experimenter/tests/integration/nimbus/pages/demo_app/frontend.py
+++ experimenter/tests/integration/nimbus/pages/demo_app/frontend.py
@@ -12,16 +12,16 @@ class DemoAppPage(Base):
         xpath_conditions = " | ".join([f".='{text}'" for text in text_list])
         xpath = f"//h1[{' or '.join(xpath_conditions.split(' | '))}]"
 
-        return self.wait_for_and_find_element((By.XPATH, xpath), description=None)
+        return self.wait_for_and_find_element(By.XPATH, xpath, description=None)
 
     def fill_and_send_form_data(self, client_id, context):
         client_xpath = "//input[@placeholder='Client ID']"
         client_id_input = self.wait_for_and_find_element(
-            (By.XPATH, client_xpath), description=None
+            By.XPATH, client_xpath, description=None
         )
         context_xpath = "//input[@placeholder='Context']"
         context_input = self.wait_for_and_find_element(
-            (By.XPATH, context_xpath), description=None
+            By.XPATH, context_xpath, description=None
         )
         time.sleep(10)
 
@@ -31,7 +31,7 @@ class DemoAppPage(Base):
     def click_send_my_details(self):
         details_xpath = "//button[contains(text(), 'Send My Details')]"
         send_details_button = self.wait_for_and_find_element(
-            (By.XPATH, details_xpath), description=None
+            By.XPATH, details_xpath, description=None
         )
 
         send_details_button.click()
